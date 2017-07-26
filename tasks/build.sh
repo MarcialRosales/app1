7565ebbf34282415d5ea34abd6ffe66b87cc826e
Marcial Rosales <mrosales@pivotal.io>
2017-07-26 12:37:11 +0200

    call mvn package instead

--- tasks/build.sh
+++ tasks/build.sh
@@ -4,9 +4,9 @@ set -eu
 echo "Calling Maven ..."
 
 cd source-code
-#mvn package
-mkdir target
-touch target/demo-0.0.1-SNAPSHOT.jar
+mvn package
+#mkdir target
+#touch target/demo-0.0.1-SNAPSHOT.jar
 
 echo "Copying artifacts ..."
 cp -r target/*.jar ../build-artifact
