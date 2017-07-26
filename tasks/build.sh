00b646610fd6f229554d21a397b6b7c481471b8b
Marcial Rosales <mrosales@pivotal.io>
2017-07-26 17:15:00 +0200

    Print version to use from build task

--- tasks/build.sh
+++ tasks/build.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 set -eu
-echo "Calling Maven ..."
+
+version=`cat version/number`
+echo "Using version $version"
 
 cd source-code
+echo "Calling Maven ..."
 mvn package
 #mkdir target
 #touch target/demo-0.0.1-SNAPSHOT.jar
+
 
 echo "Copying artifacts ..."
 cp -r target/*.jar ../build-artifact
